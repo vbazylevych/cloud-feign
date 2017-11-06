51161fd96ba78f6345d1690797734ecc29ee7125
vbazylevych <vbazylevych@playtika.com>
2017-11-06 17:46:21 +0200

    exclude

--- src/main/java/com/playtika/qa/carsshop/CarsShopApplication.java
+++ src/main/java/com/playtika/qa/carsshop/CarsShopApplication.java
@@ -5,14 +5,16 @@ import org.springframework.boot.actuate.autoconfigure.*;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
 
 
 @SpringBootApplication
@@ -29,7 +31,11 @@ import org.springframework.boot.autoconfigure.web.WebClientAutoConfiguration;
         MetricExportAutoConfiguration.class,
         PublicMetricsAutoConfiguration.class,
         WebClientAutoConfiguration.class,
-        HealthIndicatorAutoConfiguration.class
+        HealthIndicatorAutoConfiguration.class,
+        MultipartAutoConfiguration.class,
+        SpringDataWebAutoConfiguration.class,
+        WebSocketAutoConfiguration.class,
+        TransactionAutoConfiguration.class
 
 })
 public class CarsShopApplication {
