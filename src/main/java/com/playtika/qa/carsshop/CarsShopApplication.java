768a15bfec407e678ae38d9d8dada8f7a7e0f815
vbazylevych <vbazylevych@playtika.com>
2017-11-07 12:29:11 +0200

    exclude

--- src/main/java/com/playtika/qa/carsshop/CarsShopApplication.java
+++ src/main/java/com/playtika/qa/carsshop/CarsShopApplication.java
@@ -1,43 +1,25 @@
 package com.playtika.qa.carsshop;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.*;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
-import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
-import org.springframework.boot.autoconfigure.h2.H2ConsoleAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.WebClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
 
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class,
-        EndpointMBeanExportAutoConfiguration.class,
-        FreeMarkerAutoConfiguration.class,
-        H2ConsoleAutoConfiguration.class,
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,
         JmxAutoConfiguration.class,
-        MetricRepositoryAutoConfiguration.class,
-        SpringApplicationAdminJmxAutoConfiguration.class,
-        AuditAutoConfiguration.class,
-        EndpointAutoConfiguration.class,
-        MetricExportAutoConfiguration.class,
-        PublicMetricsAutoConfiguration.class,
-        WebClientAutoConfiguration.class,
-        HealthIndicatorAutoConfiguration.class,
         MultipartAutoConfiguration.class,
-        SpringDataWebAutoConfiguration.class,
-        WebSocketAutoConfiguration.class,
-        TransactionAutoConfiguration.class
-
-})
+        PropertyPlaceholderAutoConfiguration.class,
+        SpringApplicationAdminJmxAutoConfiguration.class,
+        WebClientAutoConfiguration.class,
+        WebSocketAutoConfiguration.class})
 public class CarsShopApplication {
 
     public static void main(String[] args) {
