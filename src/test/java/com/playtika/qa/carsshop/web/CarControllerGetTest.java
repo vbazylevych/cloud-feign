1c68c030e0524d06b4ab6f69368ee0287975ed0c
vbazylevych <608levlerka>
2017-11-12 23:52:11 +0200

    mvc

--- src/test/java/com/playtika/qa/carsshop/web/CarControllerGetTest.java
+++ src/test/java/com/playtika/qa/carsshop/web/CarControllerGetTest.java
@@ -3,27 +3,18 @@ package com.playtika.qa.carsshop.web;
 import com.playtika.qa.carsshop.domain.Car;
 import com.playtika.qa.carsshop.domain.CarInStore;
 import com.playtika.qa.carsshop.service.CarService;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import static java.util.Arrays.asList;
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -33,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(CarController.class)
-public class CarControllerIntegrationTest {
+public class CarControllerGetTest {
     @Autowired
     private MockMvc mockMvc;
 
@@ -41,23 +32,17 @@ public class CarControllerIntegrationTest {
     private CarService carService;
 
     @Test
-    public void addCar() throws Exception {
-        Car car = new Car(10, "e", "e", 1);
-
-
-        CarInStore carInStore = new CarInStore(car, 10, "cont");
-
-        when(carService.addCarToStore(carInStore)).thenReturn(carInStore);
-        String jsonString = "{\"enginePower\": 10, \"color\": \"e\", \"model\": \"e\", \"id\": 1 } ";
-        String contentAsString = mockMvc.perform(post("/cars?price=10&contact=cont").content(jsonString).accept(MediaType.APPLICATION_JSON_UTF8).contentType(MediaType.APPLICATION_JSON_UTF8))
+    public void getCar() throws Exception {
+                Map<String, Object> response = new HashMap<>();
+        response.put("price",1);
+        response.put("contact", "cont");
+        when(carService.getCar(1)).thenReturn(response);
+        mockMvc.perform(get("/cars/1").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json;charset=UTF-8"))
-                .andReturn().getResponse().getContentAsString();
-        Assert.assertEquals("1", contentAsString);
-
-
-        // .andReturn().getResponse().getContentAsString().compareTo("");
-
+                .andExpect(jsonPath("price").value("1"))
+                .andExpect(jsonPath("contact").value("cont"));
     }
+
 
 }
---
+++ src/test/java/com/playtika/qa/carsshop/web/CarControllerPostTest.java
@@ -0,0 +1,57 @@
+package com.playtika.qa.carsshop.web;
+
+import com.playtika.qa.carsshop.domain.Car;
+import com.playtika.qa.carsshop.domain.CarInStore;
+import com.playtika.qa.carsshop.service.CarService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CarController.class)
+public class CarControllerPostTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CarService carService;
+
+    @Test
+    public void addCar() throws Exception {
+        Car car = new Car(1, "", "", 1);
+        CarInStore carInStore = new CarInStore(car, 10, "cont");
+
+        when(carService.addCarToStore(carInStore)).thenReturn(carInStore);
+        String jsonString = "{\"enginePower\": 1, \"color\": \"\", \"model\": \"\", \"id\": 1 } ";
+        mockMvc.perform(post("/cars?price=10&contact=cont").content(jsonString).accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().json("1"));
+    }
+
+    @Test
+    public void ifContactParamIsMissedThrowException() throws Exception {
+        mockMvc.perform(post("/cars?price=10").content("{}").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+    }
+    @Test
+    public void ifPriceParamIsMissedThrowException() throws Exception {
+        mockMvc.perform(post("/cars?contact=jj").content("{}").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+    }
+
+
+
+}
