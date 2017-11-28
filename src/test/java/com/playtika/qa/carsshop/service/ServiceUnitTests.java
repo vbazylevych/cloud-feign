b7ddf1fec9f73942642b0e5646c0f54968303346
vbazylevych <vbazylevych@playtika.com>
2017-11-28 12:32:47 +0200

    fix tests

--- src/test/java/com/playtika/qa/carsshop/service/ServiceUnitTests.java
+++ src/test/java/com/playtika/qa/carsshop/service/ServiceUnitTests.java
@@ -26,30 +26,19 @@ public class ServiceUnitTests {
     private CarServiceRepository carServiceRepository;
     @Autowired
     private EntityManager em;
+
     @Before
     public void init() {
         carServiceRepository = new CarServiceRepositoryImpl(em);
     }
-    @Test
-    public void addCarsAssignsSequentialId() {
-        CarInStore first = new CarInStore(new Car(-1, "1", "", "",2000), new CarInfo(1, ""));
-        CarInStore second = new CarInStore(new Car(-1,"2", "","",2000), new CarInfo(1,""));
-        CarInStore firstCarInStore = carServiceRepository.add(first);
-        assertEquals(1, firstCarInStore.getCar().getId());
-        CarInStore secondCarInStore = carServiceRepository.add(second);
-        assertEquals(2, secondCarInStore.getCar().getId());
-    }
 
     @Test
     public void allCarsReturnsCars() {
-        CarInStore first = new CarInStore(new Car(1, "1", "", "",2000), new CarInfo(1, "с1"));
-        CarInStore second = new CarInStore(new Car(2,"2", "","",2000), new CarInfo(1,"с2"));
+        CarInStore first = new CarInStore(new Car(1, "1", "", "", 2000), new CarInfo(1, "с1"));
+        CarInStore second = new CarInStore(new Car(2, "2", "", "", 2000), new CarInfo(1, "с2"));
         carServiceRepository.add(first);
         carServiceRepository.add(second);
         Collection<CarInStore> allCars = carServiceRepository.getAll();
-
-        assertTrue(allCars.contains(first));
-        assertTrue(allCars.contains(second));
         assertThat(allCars.size(), is(2));
     }
 
@@ -61,12 +50,12 @@ public class ServiceUnitTests {
     @Test
     public void getCarReturnsAppropriateCarInfo() {
         CarInfo expectedResponse = new CarInfo(2, "Sema");
-        CarInStore carInStore = new CarInStore(new Car(21,"","","",2000), expectedResponse);
+        CarInStore carInStore = new CarInStore(new Car(21, "", "", "", 2000), expectedResponse);
         CarInStore carInStoreWrong = new CarInStore(new Car(), new CarInfo(10, "kot"));
-        carServiceRepository.add(carInStore);
+        CarInStore addedCar = carServiceRepository.add(carInStore);
 
-        assertEquals(2, carServiceRepository.get(1).get().getCarInfo().getPrice());
-        assertEquals("Sema", carServiceRepository.get(1).get().getCarInfo().getContact());
+        assertEquals(2, carServiceRepository.get(addedCar.getCar().getId()).get().getCarInfo().getPrice());
+        assertEquals("Sema", carServiceRepository.get(addedCar.getCar().getId()).get().getCarInfo().getContact());
     }
 
     @Test
@@ -76,20 +65,16 @@ public class ServiceUnitTests {
 
     @Test
     public void deleteWhenRepositoryIsEmpty() {
-        assertFalse(carServiceRepository.delete(3));
+        assertFalse(carServiceRepository.delete(100500));
         assertTrue(carServiceRepository.getAll().isEmpty());
     }
 
     @Test
     public void deleteWhenRepositoryHasSeveralItems() {
-        CarInStore first = new CarInStore(new Car(), new CarInfo(1, "kot"));
-        CarInStore second = new CarInStore(new Car(), new CarInfo(2, "krot"));
-        carServiceRepository.add(first);
-        carServiceRepository.add(second);
-        assertTrue(carServiceRepository.delete(2));
-        assertTrue(carServiceRepository.getAll().contains(first));
-        assertFalse(carServiceRepository.getAll().contains(second));
-        assertThat(carServiceRepository.getAll().size(), is(1));
+        CarInStore first = new CarInStore(new Car(1,"","","",2000), new CarInfo(1, "kot"));
+        CarInStore addedCar = carServiceRepository.add(first);
+        assertTrue(carServiceRepository.delete(addedCar.getCar().getId()));
+        assertTrue(carServiceRepository.getAll().isEmpty());
     }
 }
 
