e9eb73e6f06e7b2339007a46626d1bbe94f66a32
vbazylevych <vbazylevych@playtika.com>
2017-12-08 18:34:36 +0200

    ads tests

--- src/test/java/com/playtika/qa/carsshop/dao/AdsRepositoryTest.java
+++ src/test/java/com/playtika/qa/carsshop/dao/AdsRepositoryTest.java
@@ -2,24 +2,36 @@ package com.playtika.qa.carsshop.dao;
 
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.playtika.qa.carsshop.dao.entity.AdsEntity;
 import com.playtika.qa.carsshop.dao.entity.AdsEntityRepository;
 import com.playtika.qa.carsshop.dao.entity.CarEntity;
-import org.hamcrest.CoreMatchers;
+import com.playtika.qa.carsshop.dao.entity.UserEntity;
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.springframework.test.annotation.Commit;
 
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.junit.Assert.assertTrue;
 
 public class AdsRepositoryTest extends AbstractDaoTest<AdsEntityRepository> {
     @Test
-    @DataSet(value="filled-ads-table.xml", disableConstraints = true, useSequenceFiltering = false)
+    @DataSet("empty-car.xml")
+    @ExpectedDataSet("seved-ads-table.xml")
+    @Commit
+    public void adsMayBeStored() {
+        CarEntity car = new CarEntity("xxx", "opel", 2000, "red");
+        UserEntity user = new UserEntity("kot", "krot", "con1");
+        AdsEntity ads = new AdsEntity(user, car, 100500, null);
+        dao.save(ads);
+    }
+
+    @Test
+    @DataSet(value = "filled-ads-table.xml", disableConstraints = true, useSequenceFiltering = false)
     @DBUnit(allowEmptyFields = true)
     public void findByCarIdReturnsOpenAdsIfPresent() {
         List<AdsEntity> result = dao.findByCarIdAndDealIsNull(1);
@@ -27,9 +39,20 @@ public class AdsRepositoryTest extends AbstractDaoTest<AdsEntityRepository> {
     }
 
     @Test
-    @DataSet(value="empty-ads-table.xml", disableConstraints = true, useSequenceFiltering = false)
+    @DataSet(value = "empty-ads-table.xml", disableConstraints = true, useSequenceFiltering = false)
     @DBUnit(allowEmptyFields = true)
     public void findByCarIdReturnsEmptyListIfTableIsEmpty() {
         assertThat(dao.findByCarIdAndDealIsNull(1), Matchers.is(empty()));
     }
+
+    @Test
+    @DataSet(value = "filled-ads-table.xml", disableConstraints = true, useSequenceFiltering = false)
+    @DBUnit(allowEmptyFields = true)
+    public void allOpenedAdsWillBeReturned() {
+        List<AdsEntity> result = dao.findByDealIsNull();
+        assertTrue(result.size() == 2);
+        assertTrue(result.get(0).getDeal().isEmpty());
+        assertTrue(result.get(1).getDeal().isEmpty());
+    }
+
 }
